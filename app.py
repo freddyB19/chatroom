fbb48104ee7ee8c2839f9b0c10aaf0f06fc9be03
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-15 01:08:03 -0400

    fix: importando modulo eventlet

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import eventlet
+
+eventlet.monkey_patch()
+
 import settings 
 from factory import create_app
 from factory import create_app_socket
-
 
 app = create_app(config_object = settings.configs['deploy'])
 socketio = create_app_socket(app)
