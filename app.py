be3e0389bb7d059950e6256bd4d459ef70819167
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-15 00:12:13 -0400

    update: archivo de ejecución de despliegue

--- app.py
+++ app.py
@@ -7,9 +7,3 @@ app = create_app(config_object = settings.configs['deploy'])
 socketio = create_app_socket(app)
 
 
-if __name__ == '__main__':
-	socketio.run(app, use_reloader = True)
-
-# gunicorn -b 127.0.0.1:5000 --worker-class eventlet app:app
-
-
